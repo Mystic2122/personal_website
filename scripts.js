b6aa2dc35fd47732b351280214968216328528f5
Nick Wendt <102791771+Mystic2122@users.noreply.github.com>
2025-10-29 19:12:37 -0700

    done working for tonight

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Change pfp to steve on hover
 const profileImg = document.getElementById('profile-img');
 
 profileImg.addEventListener("mouseenter", () => {
@@ -6,4 +7,14 @@ profileImg.addEventListener("mouseenter", () => {
 
 profileImg.addEventListener("mouseleave", () => {
   profileImg.src = "images/profile.jpg";
+});
+
+
+window.addEventListener("scroll", function() {
+    const nav = document.querySelector("nav");
+    if (window.scrollY > 0) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
 });
