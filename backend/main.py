958bd44a45f0dd22cb85cf0591c0947ab73f13c8
Nick Wendt <102791771+Mystic2122@users.noreply.github.com>
2026-08-24 13:33:12 -0700

    added website as allowed origin

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://mystic2122.github.io",
+    ],
     allow_credentials=False,
     allow_methods=["GET", "POST"],
     allow_headers=["Content-Type"],
